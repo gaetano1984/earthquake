c60bb064e134ec89c08b8502aa6833b9eded444d
gaetano campanella <gaetanocampanella@MBPdigaetano2.lan>
2019-01-28 23:38:54 +0100

    sostituito bootstrap locale con cdn

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,8 +3,8 @@
 <head>
 	<title>Terremoti</title>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="{{url('../node_modules/bootstrap/dist/css/bootstrap.min.css')}}">
-	<script type="text/javascript" src="{{url('../node_modules/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+	<script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 	<style type="text/css">
 		.validation-error{
 			color: red;
