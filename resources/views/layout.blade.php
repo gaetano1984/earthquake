ffbb0c2794feddae1efa2a2eb6a878de6b30bba1
osboxes.org <gaecamp@gmail.com>
2019-02-11 15:29:55 +0100

    issue #8 forse risolta

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Terremoti</title>
 	<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
