0d4df46a371df3737bcf37e579a6d13460d72ea0
gaetano campanella <gaetanocampanella@MBPdigaetano2.lan>
2019-02-12 22:26:25 +0100

    debug beginAtZero

--- resources/views/earthquake/stats.blade.php
+++ resources/views/earthquake/stats.blade.php
@@ -49,7 +49,7 @@
 						scales:{
 							yAxes: [{
 								ticks:{
-									beginatZero: true
+									beginAtZero: true
 								}
 							}]
 						}
@@ -71,7 +71,7 @@
 						scales:{
 							yAxes: [{
 								ticks:{
-									beginatZero: true
+									beginAtZero: true
 								}
 							}]
 						}
