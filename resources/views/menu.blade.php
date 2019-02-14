cc3808e79ef11d10af66145ca2db3fb556446be4
gaetano campanella <gaetanocampanella@MBPdigaetano2.lan>
2019-02-14 23:49:18 +0100

    issue #9

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,20 +1,26 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 	<a class="navbar-brand" href="#">{{__('earthquake.earthquake')}}</a>
-	<ul class="navbar-nav mr-auto">
-		<li class="nav-item">
-			<a class="nav-link" href="{{route('quake_list')}}">
-				{{__('earthquake.menu.most_recent')}}
-			</a>
-		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="{{route('quake_stats')}}">
-				{{__('earthquake.menu.stats')}}
-			</a>
-		</li>
-	</ul>
-	<span class="my-2 my-lg-0" style="color:white; "><a href="{{url('user_profile')}}">Bentornato {{$user['name']}} {{$user['surname']}}</a></span>&nbsp; 
-	<form method="POST" action="{{url('logout')}}">
-		{{ csrf_field() }}
-		<button class="btn btn-outline-danger">Logout</button>
-	</form>
+	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbar-menu" aria-controls="navbar-menu">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="navbar-collapse collapse" id="navbar-menu">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="{{route('quake_list')}}">
+					{{__('earthquake.menu.most_recent')}}
+				</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="{{route('quake_stats')}}">
+					{{__('earthquake.menu.stats')}}
+				</a>
+			</li>
+		</ul>	
+		<span class="my-2 my-lg-0" style="color:white; "><a href="{{url('user_profile')}}">Bentornato {{$user['name']}} {{$user['surname']}}</a></span>&nbsp; 
+		<form method="POST" action="{{url('logout')}}">
+			{{ csrf_field() }}
+			<button class="btn btn-outline-danger">Logout</button>
+		</form>
+	</div>
 </nav>
