6b0b22bfa6beb774355d9e63943710b0797fa0cb
gaetano campanella <gaetanocampanella@MBPdigaetano2.lan>
2019-03-20 23:19:41 +0100

    merge del conflict

--- resources/lang/it/api.php
+++ resources/lang/it/api.php
@@ -1,24 +1,24 @@
 <?php 
 
-	return [	
-		'validation' => [
-			'url' => [
-                  	'required_without' => 'La url è obbligatoria se non viene specificato l\'indirizzo IP'
-                  	,'url' => 'La url deve essere nel formato url'
+      return [    
+            'validation' => [
+                  'url' => [
+                        'required_without' => 'La url è obbligatoria se non viene specificato l\'indirizzo IP'
+                        ,'url' => 'La url deve essere nel formato url'
                   ]
                   ,'ip' => [
                         'required_without' => 'L\'indirozzo ip è obbligatorio se non viene specificato una url'
                         ,'ip' => 'L\'indirizzo ip deve essere nel formato ip'
                   ]
                   ,'key' => [
-              		'required' => 'La key è obbligatoria'
-                  	,'max' => 'La key deve essere lunga massimo 255 caratteri'
+                        'required' => 'La key è obbligatoria'
+                        ,'max' => 'La key deve essere lunga massimo 255 caratteri'
                   ]
                   ,'secret' => [
-                  	'required' => 'La secret è obbligatoria'
-                  	,'max' => 'La secret deve essere lunga massimo 255 caratteri'
+                        'required' => 'La secret è obbligatoria'
+                        ,'max' => 'La secret deve essere lunga massimo 255 caratteri'
                   ]
-		],
-	];
+            ],
+      ];
 
  ?>
