34c3a648d18feb5a6a365e08d2922395f1418470
osboxes.org <gaecamp@gmail.com>
2019-03-20 12:51:56 +0100

    correzioni tab

--- resources/lang/it/api.php
+++ resources/lang/it/api.php
@@ -3,17 +3,17 @@
 	return [	
 		'validation' => [
 			'url' => [
-            	'required' => 'La url è obbligatoria'
-            	,'url' => 'La url deve essere nel formato url oppure un indirizzo IP'
-            ]
-            ,'key' => [
-        		'required' => 'La key è obbligatoria'
-            	,'max' => 'La key deve essere lunga massimo 255 caratteri'
-            ]
-            ,'secret' => [
-            	'required' => 'La secret è obbligatoria'
-            	,'max' => 'La secret deve essere lunga massimo 255 caratteri'
-            ]
+                  	'required' => 'La url è obbligatoria'
+                  	,'url' => 'La url deve essere nel formato url oppure un indirizzo IP'
+                  ]
+                  ,'key' => [
+              		'required' => 'La key è obbligatoria'
+                  	,'max' => 'La key deve essere lunga massimo 255 caratteri'
+                  ]
+                  ,'secret' => [
+                  	'required' => 'La secret è obbligatoria'
+                  	,'max' => 'La secret deve essere lunga massimo 255 caratteri'
+                  ]
 		],
 	];
 
